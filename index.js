c7908232d113e077e1acf2fd65d57e1786c2c89c
Mohaiminul-Islam-Bhuiyan <rafe.aust@gmail.com>
2022-11-10 16:08:04 +0600

    load services added

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
 const express = require('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 
 const app = express()
 const port = process.env.PORT || 5000;
 
 // middlewire
 app.use(cors())
+
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASSWORD);
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.a0wgsnp.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const serviceCollection = client.db('shuvro').collection('services')
+
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query)
+            const services = await cursor.toArray()
+            res.send(services)
+        })
+
+    }
+    finally {
+
+    }
+}
+
+run()
+    .catch(err => console.log(err))
+
 
 app.get('/', (req, res) => {
     res.send('shuvro server is running')
