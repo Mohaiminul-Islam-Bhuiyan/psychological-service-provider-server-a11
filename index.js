5394f61a08f67cb1a483f80e2ed85082030f936f
Mohaiminul-Islam-Bhuiyan <rafe.aust@gmail.com>
2022-11-10 21:25:27 +0600

    review adding added

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const port = process.env.PORT || 5000;
 
 // middlewire
 app.use(cors())
+app.use(express.json())
 
 console.log(process.env.DB_USER);
 console.log(process.env.DB_PASSWORD);
@@ -21,6 +22,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const serviceCollection = client.db('shuvro').collection('services')
+        const reviewCollection = client.db('shuvro').collection('reviews')
 
         app.get('/services', async (req, res) => {
             const query = {}
@@ -41,6 +43,12 @@ async function run() {
             const query = { _id: ObjectId(id) }
             const service = await serviceCollection.findOne(query)
             res.send(service)
+        })
+
+        app.post('/reviews', async (req, res) => {
+            const review = req.body
+            const result = await reviewCollection.insertOne(review)
+            res.send(result)
         })
 
     }
